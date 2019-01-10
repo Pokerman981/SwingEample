af8b524c1b5f137357a588f5c361bf8344df7297
Troy Gidney <troy.gidney@lethbridgecollege.ca>
2019-01-09 17:24:45 -0700

    Initial commit

---
+++ src/mainForm.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class mainForm {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("mainForm");
+        jFrame.setContentPane(new mainForm().panel1);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        jFrame.setVisible(true);
+    }
+
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private JButton button17;
+    private JButton button18;
+    private JButton button19;
+    private JButton button20;
+    private JButton button21;
+    private JButton button22;
+    private JButton button23;
+    private JButton button24;
+    private JButton button25;
+    private JButton button26;
+    private JButton button27;
+    private JButton button28;
+    private JButton button29;
+    private JButton button30;
+    private JButton button31;
+    private JButton button32;
+    private JButton button33;
+    private JButton button34;
+    private JButton button35;
+    private JButton button36;
+    private JButton button37;
+
+
+    public mainForm() {
+
+    }
+}
